230a964deb4a15743e5ad91d220962e6ad4b4d38
iarshtejay <arshtejay@gmail.com>
2025-08-04 18:57:20 -0300

    feat: transparent blur effect on AppBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 function Header() {
   return (
-    <AppBar position="static" color="default" elevation={1}>
+    <AppBar
+      position="sticky"
+      elevation={1}
+      color="default"
+      // Frosted Glass
+      // style={{
+      //   backgroundColor: "rgba(255, 255, 255, 0.25)",
+      //   backdropFilter: "blur(10px)",
+      //   WebkitBackdropFilter: "blur(10px)",
+      // }}
+    >
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'primary.main', fontWeight: 'bold' }}>
-          Mopify
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, color: "primary.main", fontWeight: "bold" }}
+        >
+          MOPIFY
         </Typography>
-        <Button color="inherit" href="#features">Features</Button>
-        <Button color="inherit" href="#contact">Contact</Button>
-        <Button variant="contained" color="primary">Get Started</Button>
+        <Button color="inherit" href="#features">
+          Features
+        </Button>
+        <Button color="inherit" href="#contact">
+          Contact
+        </Button>
+        <Button variant="contained" color="primary">
+          Get Started
+        </Button>
       </Toolbar>
     </AppBar>
   );
