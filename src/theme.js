6e14c3a4442223e820d0572e871d45f10963fcf9
iarshtejay <arshtejay@gmail.com>
2025-09-05 23:29:09 -0300

    chore: fix custom font not displaying in safari

--- src/theme.js
+++ src/theme.js
@@ -1,29 +1,29 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
       /*main: '#B03623', // Dark brown*/
-      main: '#B0462F', // Match with logo pdf
+      main: "#B0462F", // Match with logo pdf
     },
     secondary: {
-      main: '#F9E2bc', // Very light yellow
+      main: "#F9E2bc", // Very light yellow
     },
     background: {
-      default: '#FFFFFF', // A very light grey for the page background
+      default: "#FFFFFF", // A very light grey for the page background
     },
   },
   typography: {
     fontFamily: [
-      '-apple-system',
-      'Omnes Regular',
-      'BlinkMacSystemFont',
+      "Omnes Regular",
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-    ].join(','),
+      "Arial",
+      "sans-serif",
+    ].join(","),
   },
 });
 
